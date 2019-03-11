06a4743878ae3292fc6d90a367cf9df49d423d01
Amanda Maia <amandampessanha@hotmail.com>
2019-03-11 14:58:01 -0300

    Update Main.java

--- src/PlayListMusic/Main.java
+++ src/PlayListMusic/Main.java
@@ -14,17 +14,17 @@ public class Main {
 		Playlist playlist1 = new Playlist();
 			
 		//ATRIBUI OS NOVOS OBJETOS MUSIC NAS SUAS RESPECTIVAS VARIAVEIS
-		musics[0] = new Music("Agua de beber", "Antonio Carlos Jobim", "Astrud Gilberto", 140, 0);
-		musics[1] = new Music ("O mar serenou", "Candeia", "Clara Nunes", 179, 0);
-		musics[2] = new Music ("Rapaz Folgado", "Noel Rosa", "Martinho da vila e Mart'nália", 180, 0);
+		musics[0] = new Music("Agua de beber", "Antonio Carlos Jobim", "Astrud Gilberto", 140);
+		musics[1] = new Music ("O mar serenou", "Candeia", "Clara Nunes", 179);
+		musics[2] = new Music ("Rapaz Folgado", "Noel Rosa", "Martinho da vila e Mart'nÃ¡lia", 180);
 		
 		//ATRIBUI OS NOVOS OBJETOS VIDEO NAS SUAS RESPECTIVASA VARIAVEIS
 		videos[0] = new Video ( "Musica", "Despacito", "Luis Fonsi", 5926796, 280);
 		videos[1] = new Video ("Musica", "Gangnam Style", "Officialpsy", 3276192, 252);
 		
-		//LABEL PARA AUXILIAR NO QUE ESTÁ SENDO PRINTADO NA TELA
+		//LABEL PARA AUXILIAR NO QUE ESTA SENDO PRINTADO NA TELA
 		System.out.println("===================================================");
-		System.out.println("As músicas adicionadas foram: ");
+		System.out.println("As musicas adicionadas foram: ");
 		
 		//FOR PARA PERCORRER O ARRAY TOTAL, PRINTAR E ADICIONAR AS MUSICAS NA PLAYLIST
 		for (int i = 0; i < musics.length; i++) {
@@ -36,11 +36,11 @@ public class Main {
 		
 		//PRINTA NA TELA O TEMPO DE DURACAO DA PLAYLIST COM APENAS AS MUSICAS
 		System.out.println("===================================================");		
-		System.out.println("Duração da playlist com as músicas: " + playlist1.toString());
+		System.out.println("Duracao da playlist com as musicas: " + playlist1.toString());
 		
-		//LABEL PARA AUXILIAR NO QUE ESTÁ SENDO PRINTADO NA TELA
+		//LABEL PARA AUXILIAR NO QUE ESTÃ SENDO PRINTADO NA TELA
 		System.out.println("===================================================");
-		System.out.println("Os vídeos adicionados foram: ");
+		System.out.println("Os videos adicionados foram: ");
 		
 		//FOR PARA PERCORRER O ARRAY TOTAL, PRINTAR E ADICIONAR OS VIDEOS NA PLAYLIST
 		for (int i = 0; i < videos.length; i++) {
@@ -50,9 +50,9 @@ public class Main {
 			playlist1.addEntertainment(videos[i]);
 		}
 		
-		//LABEL PARA AUXILIAR NO QUE ESTÁ SENDO PRINTADO NA TELA
+		//LABEL PARA AUXILIAR NO QUE ESTA SENDO PRINTADO NA TELA
 		System.out.println("===================================================");		
-		System.out.print("Duração da playlist com as músicas e os vídeos, lembrando que os vídeos podem conter ou não propaganadas: ");
+		System.out.print("Duracao da playlist com as musicas e os videos, lembrando que os vÃ­deos podem conter ou nao propaganadas: ");
 
 		//PRINTA NA TELA O TEMPO DE DURACAO DA PLAYLIST
 		System.out.println(playlist1.toString());
